f1a2746c52ab04edff75dbed7304532d45a2890e
jimi.quintero <jimi.quintero@bigholding.com.co>
2021-05-10 10:02:15 -0500

    Crud Finalizado

--- js/crud_fecth.js
+++ js/crud_fecth.js
@@ -19,6 +19,11 @@ const getAll = async () => {
     json.forEach((el) => {
       $template.querySelector(".name").textContent = el.name;
       $template.querySelector(".constellation").textContent = el.constellation;
+      $template.querySelector(".edit").dataset.id = el.id;
+      $template.querySelector(".edit").dataset.name = el.name;
+      $template.querySelector(".edit").dataset.constellation = el.constellation;
+      $template.querySelector(".delete").dataset.id = el.id;
+
       let $clone = d.importNode($template, true);
       $fragment.appendChild($clone);
     });
@@ -35,3 +40,120 @@ const getAll = async () => {
 };
 
 d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", async (e) => {
+  if (e.target === $form) {
+    e.preventDefault();
+
+    if (!e.target.id.value) {
+      // Create - POST
+      try {
+        let options = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json; charset= utf-8",
+            },
+            body: JSON.stringify({
+              name: e.target.name.value,
+              constellation: e.target.constellation.value,
+            }),
+          },
+          response = await fetch("http://localhost:5000/santos", options),
+          json = response.json();
+
+        if (!response.ok)
+          throw {
+            status: response.status,
+            statusText: response.statusText,
+          };
+
+        location.reload();
+      } catch (error) {
+        let message = error.statusText || "Ocurrio un error !!";
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p style="color: red"><b>Error ${error.status}: ${message}</b></p>`
+        );
+      }
+    } else {
+      // Update - PUT
+      try {
+        let options = {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json; charset= utf-8",
+            },
+            body: JSON.stringify({
+              name: e.target.name.value,
+              constellation: e.target.constellation.value,
+            }),
+          },
+          response = await fetch(
+            `http://localhost:5000/santos/${e.target.id.value}`,
+            options
+          ),
+          json = response.json();
+
+        if (!response.ok)
+          throw {
+            status: response.status,
+            statusText: response.statusText,
+          };
+
+        location.reload();
+      } catch (error) {
+        let message = error.statusText || "Ocurrio un error !!";
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p style="color: red"><b>Error ${error.status}: ${message}</b></p>`
+        );
+      }
+    }
+  }
+});
+
+d.addEventListener("click", async (e) => {
+  if (e.target.matches(".edit")) {
+    $title.textContent = "Editar Santo";
+    $form.name.value = e.target.dataset.name;
+    $form.constellation.value = e.target.dataset.constellation;
+    $form.id.value = e.target.dataset.id;
+  }
+
+  if (e.target.matches(".delete")) {
+    let isDelete = confirm(
+      `¿Estás seguro de eliminar el id ${e.target.dataset.id} ?`
+    );
+
+    if (isDelete) {
+      // Eliminar - DELETE
+      try {
+        let options = {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json; charset= utf-8",
+            },
+          },
+          response = await fetch(
+            `http://localhost:5000/santos/${e.target.dataset.id}`,
+            options
+          ),
+          json = response.json();
+
+        if (!response.ok)
+          throw {
+            status: response.status,
+            statusText: response.statusText,
+          };
+
+        location.reload();
+      } catch (error) {
+        let message = error.statusText || "Ocurrio un error !!";
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p style="color: red"><b>Error ${error.status}: ${message}</b></p>`
+        );
+      }
+    }
+  }
+});
